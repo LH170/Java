f91143641671c0506df03b93a9239055206d79fd
LH170 <loganjameshintze@gmail.com>
2023-04-18 09:11:23 +0100

    Initial Git Commit

---
+++ OOP Assignment/src/GraphicsSystem.java
@@ -0,0 +1,16 @@
+import uk.ac.leedsbeckett.oop.LBUGraphics;
+
+public class GraphicsSystem extends LBUGraphics {
+
+	public static void main (String[] args) {
+		new GraphicsSystem();
+	}
+	public GraphicsSystem() {
+	}
+	@Override
+	public void processCommand(String arg0) {
+		// TODO Auto-generated method stub
+		about();
+	}
+
+}
