83c0b7d7fe1a85c21dd73e47226dd47e40cc94e8
LH170 <loganjameshintze@gmail.com>
2023-04-18 09:07:48 +0100

    Initial Git Commit

---
+++ Student Grades/src/Student_Grades.java
@@ -0,0 +1,16 @@
+
+public class Student_Grades {
+	public static void main (String []args)
+	{
+		System.out.println ("///////////////////" + "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		System.out.println ("==          Student Points         ==");
+		System.out.println ("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" + "//////////////////");
+		System.out.println ("\nName            Lab    Bonus   Total");
+		System.out.println ("----            ---    -----   -----");
+		System.out.println ("Luke            45     10      55");
+		System.out.println ("Ben             35     10      45");
+		System.out.println ("Tom             25     10      35");
+		System.out.println ("Jay             15     10      25");
+		System.out.println ("Kyle            5      10      15");
+	}
+}
